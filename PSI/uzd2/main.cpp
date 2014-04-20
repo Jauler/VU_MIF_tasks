0b321ecb336eed5948d9e9945c70f3e31f0183f7
Rytis <rytis@elektromotus.lt>
2014-04-20 23:30:05 +0300

    added situation description

--- PSI/uzd2/main.cpp
+++ PSI/uzd2/main.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+/*
+	Situacija:
+		Turime įmone su darbuotojais, įvairūs darbuotojai gali atlikti
+		skirtingus darbus, tarkime, kad tai IT įmonė, tad darbuotojas
+		gali atlikti šiuos veiksmus; "Design, Code, Test". Tai yra tarsi
+		mūsų Rolė. Kartais darbuotojai gali atlikti kelius veiksmus iškarto
+		pvz gali projektuoti ir programuoti ar pan. Projektuotojai turi 
+		specialų veiksmą - konsultuotis su klientais. Kiekvienas darbuotojas
+		turi savybę "produktyvumas" ir speciali rolė "manager" gali ją keisti
+		priklausomai nuo to kas ir kaip vyksta. Taip pat "manager" rolė turi
+		specialią galimybę nustatyti šio darbuotojo nuotaiką, o ši įtakos
+		darbuotojų produktyvumo vertinimus.
+*/
+
 int main(void)
 {
 	cout << "Hello World!\n";
