006561eb8366a242b34b63c153dde659ec5d8a81
Rytis <rytis@elektromotus.lt>
2014-04-20 21:22:02 +0300

    changed some function to be protected as they should

--- PSI/uzd1/tm/TM_SpeedFirstDeadlineEstimator.h
+++ PSI/uzd1/tm/TM_SpeedFirstDeadlineEstimator.h
@@ -6,11 +6,12 @@
 #include "../app/DeadlineEstimator.h"
 
 class TM_SpeedFirstDeadlineEstimator : public DeadlineEstimator {
-
-public:
+protected:
 	virtual int calcWorkingTime(int difficulty){};
 	virtual int calcWorkingSpeed(int difficulty){};
 	virtual int calcRequiredWorkingDays(int difficulty){};
+
+public:
 
 	int calcTimeToDeadline(int difficulty);
 
