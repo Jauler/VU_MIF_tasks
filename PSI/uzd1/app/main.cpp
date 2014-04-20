b971223d7fe6fffb9e738d2b659441564baa556f
Rytis <rytis@elektromotus.lt>
2014-04-20 21:24:36 +0300

    fixed stupid bug on range check

--- PSI/uzd1/app/main.cpp
+++ PSI/uzd1/app/main.cpp
@@ -30,6 +30,7 @@ int main(void)
 	cin >> difficulty;
 	if(difficulty <= 0 || difficulty > 10){
 		cout << "    Difficulty should be in the range of [1;10]";
+		return;
 	}
 	cout << "    Difficulty level is set to " << difficulty << "\n";
 
