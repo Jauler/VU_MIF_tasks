e980e27f237945a3d38a1e1cccadb9d8dbc95e43
Rytis <rytis@elektromotus.lt>
2014-04-27 16:12:26 +0300

    using Demangling for class name comparison

--- PSI/uzd2/Decorators/WorkerDecorator.cpp
+++ PSI/uzd2/Decorators/WorkerDecorator.cpp
@@ -3,10 +3,21 @@
 #include <string>
 #include <typeinfo>
 #include "WorkerDecorator.h"
+#include <cxxabi.h>
 
 using namespace std;
 
+static string DemangleName(const char *name)
+{
+	int status;
+	char *realName = abi::__cxa_demangle(name, 0, 0, &status);
+	if(status != 0)
+		return "";
 
+	string realNameStr = string(realName);
+	delete realName;
+	return realNameStr;
+}
 
 WorkerDecorator::WorkerDecorator(Worker *_w) :
 		w(_w),
@@ -17,10 +28,9 @@ WorkerDecorator::WorkerDecorator(Worker *_w) :
 
 WorkerDecorator *WorkerDecorator::getRole(string role)
 {
-	//if we are the role we are looking for
-	string className(typeid(*this).name());
-
-	if(className.find(role) != string::npos)
+	//If we are the role we are looking for
+	string className = DemangleName(typeid(*this).name());
+	if(className == role)
 		return this;
 
 	//try to go deeper
@@ -42,8 +52,8 @@ WorkerDecorator *WorkerDecorator::removeRole(string role)
 	WorkerDecorator *tmpW = dynamic_cast<WorkerDecorator *>(w);
 
 	//If this is our role - we should return its worker
-	string className(typeid(*this).name());
-	if(className.find(role) != string::npos){
+	string className = DemangleName(typeid(*this).name());
+	if(className == role){
 		if(tmpW != NULL) ((WorkerDecorator *)w)->removeRole(role);
 		return (WorkerDecorator *)w;
 	}
@@ -54,8 +64,8 @@ WorkerDecorator *WorkerDecorator::removeRole(string role)
 		return NULL;
 
 	//Check if this is the one needed changin
-	string workerClassName(typeid(*w).name());
-	if(workerClassName.find(role) != string::npos){
+	string workerClassName = DemangleName(typeid(*w).name());
+	if(workerClassName == role){
 		w = ((WorkerDecorator *)w)->removeRole(role);
 		removeRole(role); //Do not stop here, because there might be more roles
 		return this;
