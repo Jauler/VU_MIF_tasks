b32230b85ba27f2c043051f5f5b3588de570298b
Rytis <rytis@elektromotus.lt>
2014-05-02 02:07:43 +0300

    some work at HOG done

--- ML/HOG/HOG.py
+++ ML/HOG/HOG.py
@@ -13,6 +13,7 @@ img.flags.writeable = True
 def calcXYVectors(img):
 	Gx = np.zeros(img.shape)
 	Gy = np.zeros(img.shape)
+	imgDup = img.copy()
 	for c, tmp in np.ndenumerate(img):
 		if c[0] - 1 >= 0:
 			Gx[c[0]][c[1]] = Gx[c[0]][c[1]] - img[c[0]-1][c[1]]
@@ -24,11 +25,12 @@ def calcXYVectors(img):
 		if c[1] + 1 < img.shape[1]:
 			Gy[c[0]][c[1]] = Gy[c[0]][c[1]] + img[c[0]][c[1]+1]
 
-	return (Gx, Gy)
+		imgDup[c[0]][c[1]] = Gx[c[0]][c[1]] + Gy[c[0]][c[1]]
+	return (Gx, Gy, imgDup)
 
 
 def doHist(img, Gx, Gy, x, y):
-	poses = [[x, y] for y in range(0, 16) for x in range(0, 16)]
+	poses = [[x, y] for y in range(0, 3) for x in range(0, 3)]
 	vect = np.zeros(10)
 	for i in poses:
 		if(x + i[0] >= img.shape[0]):
@@ -37,26 +39,42 @@ def doHist(img, Gx, Gy, x, y):
 			continue
 
 		#get degrees for direction and all positive values
-		deg = 180 +  math.degrees(math.atan2(Gy[x+i[0]][y+i[1]], Gx[x+i[1]][y+i[1]]))
+		deg = 180 +  math.degrees(math.atan2(Gy[x+i[0]][y+i[1]], Gx[x+i[0]][y+i[1]]))
 		#find a bin for our degrees value
 		for tmp in range(1, 11):
 			if deg < tmp * 36:
 				break;
-		vect[tmp - 1] = vect[tmp - 1] + 1
+		vect[tmp - 1] = vect[tmp - 1] + Gx[x+i[0]][y+i[1]] + Gy[x+i[0]][y+i[1]]
 	return vect
 
 def calcHists(img, Gx, Gy):
 	hists = []
-	for x in range(0, img.shape[0]/16):
+	for x in range(0, img.shape[0]/3):
 		hists.append([])
-		for y in range(0, img.shape[1]/16):
-			hists[x].append(doHist(img, Gx, Gy, x*16, y*16))
+		for y in range(0, img.shape[1]/3):
+			hists[x].append(doHist(img, Gx, Gy, x*3, y*3))
 	return hists
+
+def normaliseHist(hist):
+	e = 0.1
+	for i in range(0, len(hist)):
+		hist[i] = hist[i] * (hist[i]/np.linalg.norm(np.array(hist)) + e)
+
+	return hist
+
+
+def normaliseHists(hists):
+	myHists = []
+	for hist in hists:
+		myHists.append(normaliseHist(hist))
+
+	return myHists
 
 print img.shape
 G = calcXYVectors(img)
-hists = calcHists(img, G[0], G[1])
-print len(hists)
+hists = np.array(calcHists(img, G[0], G[1]))
+print hists
+
 
 plt.imshow(img, cmap=plt.cm.gray, interpolation='nearest')
 plt.show()
