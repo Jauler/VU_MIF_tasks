afc4bc3814991dd4d0acb46e16b8a0b002b6af83
Rytis Karpuska <rytis@elektromotus.lt>
2015-05-11 19:30:24 +0300

    final version of practice report

--- PraktikosAtaskaita/checker_itf.h
+++ PraktikosAtaskaita/checker_itf.h
@@ -1,3 +1,18 @@
+struct checker_record_s {
+	int severity;
+	int line;
+	const char *filename;
+	void *return_addr;
+	int code;
+};
+
+enum severity_e{
+	CHK_WARN,
+	CHK_ERR,
+
+	CHK_SEVERETY_CNT
+};
+
 #define chk(severity, code)  __check(severity,\
 			__FILE__,\
 			__LINE__,\
@@ -6,14 +21,11 @@
 #define chk_return(severity, code)\
 			{chk(severity, code); return code;}
 
-
 void __check(enum severity_e severity,
 		const char *filename,
 		int line,
 		void *return_addr,
 		int code);
 
-
 int CHK_cnt();
 const struct checker_record_s *CHK_getRecord(int idx);
-
